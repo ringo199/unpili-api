8a9cb427d2b216c6963992cc5222dcc12b474c5e
ylz-熊天宇 <245952797@qq.com>
2020-01-29 22:46:14 +0800

    评论详情

--- controller/comment.js
+++ controller/comment.js
@@ -2,40 +2,84 @@ const xss = require('xss')
 const { exec } = require('../db/mysql')
 const { ListModel } = require('../model/listModel')
 
-const getCommentList = async (videoId) => {
-  let sql = `
-    select comments.id, content,
+const getCommentList = async (videoId, pageNo, pageSize) => {
+  let sql = `set @commentNo=${(pageNo - 1) * pageSize};`
+  await exec(sql)
+  sql = `SELECT COUNT(*) FROM comments where commentVideo=${videoId};`
+  let total = await exec(sql)
+  total = total[0]['COUNT(*)']
+  sql = `
+    select @commentNo:=@commentNo+1 as commentNo,
+    content, comments.id,
     comments.createTime as datetime,
     users.avatar,
     users.nickname as author
     from comments
     left join users on users.id = comments.commentUser
     where commentVideo=${videoId}
-    order by comments.createTime desc;
+    order by comments.createTime asc
+    limit ${(pageNo - 1) * pageSize}, ${pageSize};
   `
 
   const rows = await exec(sql)
+
+  for (let i = 0, num = rows.length; i < num; i++) {
+    sql = `SELECT COUNT(*) FROM comments where parentComment=${rows[i].id};`
+    let replyTotal = await exec(sql)
+    replyTotal = replyTotal[0]['COUNT(*)']
+    rows[i].replyTotal = replyTotal
+  }
+
   return new ListModel({
     rows,
-    pageNo: 1,
-    pageSize: 10,
-    total: 1
+    pageNo,
+    pageSize,
+    total
   })
 }
 
-const getCommentDetail = async () => {
+const getCommentDetail = async (commentId, pageNo, pageSize) => {
+  // 重置评论楼层(伪)
+  let sql = `set @commentNo=${(pageNo - 1) * pageSize};`
+  await exec(sql)
+  // 查询评论总数
+  sql = `SELECT COUNT(*) FROM comments where parentComment=${commentId};`
+  let total = await exec(sql)
+  total = total[0]['COUNT(*)']
+  // 获取评论详细数据
+  sql = `
+    select @commentNo:=@commentNo+1 as commentNo,
+    content,
+    comments.createTime as datetime,
+    users.avatar,
+    users.nickname as author
+    from comments
+    left join users on users.id = comments.commentUser
+    where parentComment=${commentId}
+    order by comments.createTime asc
+    limit ${(pageNo - 1) * pageSize}, ${pageSize};
+  `
 
+  const rows = await exec(sql)
+
+  return new ListModel({
+    rows,
+    pageNo,
+    pageSize,
+    total
+  })
 }
 
-const saveComment =  async ({ content, videoId }, userId) => {
+const saveComment =  async ({ content, videoId, parentCommentId }, userId) => {
   content = xss(content)
+  const parentComment = xss(parentCommentId) || null
   const commentVideo = xss(videoId)
   const commentUser = userId
   const createTime = Date.now()
 
   const sql = `
       insert into comments (content, parentComment, commentVideo, commentUser, createTime)
-      values ('${content}', 'null', '${commentVideo}', '${commentUser}', '${createTime}');
+      values ('${content}', '${parentComment}', '${commentVideo}', '${commentUser}', '${createTime}');
   `
   const insertData = await exec(sql)
   return {
--- routes/comment.js
+++ routes/comment.js
@@ -23,6 +23,19 @@ router.get('/list', async function (ctx, next) {
   }
 })
 
+router.get('/detail', async function (ctx, next) {
+  try {
+    let commentId = ctx.query.commentId || ''
+    let pageNo = ctx.query.pageNo || 1
+    let pageSize = ctx.query.pageSize || 10
+
+    const listData = await getCommentDetail(commentId, pageNo, pageSize)
+    ctx.body = new SuccessModel(listData)
+  } catch (e) {
+    ctx.body = new ErrorModel(e.message)
+  }
+})
+
 router.post('/save', loginCheck, async function (ctx, next) {
   try {
     const body = ctx.request.body
